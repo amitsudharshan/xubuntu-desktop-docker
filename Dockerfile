dd14a9859606251abf69e687e42b9fe1299ec379
Amit Sudharshan <amit.sudharshan@gmail.com>
2020-05-04 02:43:13 +0000

    add xstartup and dockerfile to include it

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,9 @@ FROM ubuntu
 RUN apt-get update
 RUN DEBIAN_FRONTEND=noninteractive apt-get install -y xubuntu-desktop tightvncserver
 RUN rm -rf /var/lib/apt/lists/*
+RUN touch /root/.Xresources
+COPY xstartup /root/.vnc/xstartup
+RUN chmod +x /root/.vnc/xstartup
 
 # Define working directory.
 WORKDIR /data
--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run -it --rm -p 5901:5901 -e USER=root xubuntu-desktop bash -c "vncserver :1 -geometry 1280x800 -depth 24 && tail -F /root/.vnc/*.log"
+docker run -it --rm -v /data:/data -p 5901:5901 -e USER=root xubuntu-desktop bash -c "vncserver :1 -geometry 1280x800 -depth 24 && tail -F /root/.vnc/*.log"
